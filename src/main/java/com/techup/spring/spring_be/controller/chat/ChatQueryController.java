f34162c8574ee7a87e44024e3a2cb04659f20f4f
siyeon13 <64884565+siyeon13@users.noreply.github.com>
2025-12-29 09:26:50 +0900

    참여한 채팅방 조회

--- src/main/java/com/techup/spring/spring_be/controller/chat/ChatQueryController.java
+++ src/main/java/com/techup/spring/spring_be/controller/chat/ChatQueryController.java
@@ -1,8 +1,15 @@
 package com.techup.spring.spring_be.controller.chat;
 
+import com.techup.spring.spring_be.domain.ChatParticipant;
+import com.techup.spring.spring_be.domain.User;
 import com.techup.spring.spring_be.dto.chat.ChatMessageResponse;
+import com.techup.spring.spring_be.dto.chat.ChatParticipantResponse;
+import com.techup.spring.spring_be.repository.ChatParticipantRepository;
+import com.techup.spring.spring_be.repository.UserRepository;
 import com.techup.spring.spring_be.service.chat.ChatQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +23,14 @@ import java.util.List;
 public class ChatQueryController {
 
     private final ChatQueryService chatQueryService;
+    private final UserRepository userRepository;
+    private final ChatParticipantRepository chatParticipantRepository;
+
+    private Long getCurrentUserId(UserDetails userDetails) {
+        User user = userRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
+        return user.getId();
+    }
 
     @GetMapping("/{communityId}")
     public List<ChatMessageResponse> getMessages(
@@ -23,5 +38,13 @@ public class ChatQueryController {
     ) {
         return chatQueryService.getMessages(communityId);
     }
+
+    @GetMapping("/me/participant")
+    public List<ChatParticipantResponse> myChatParticipant(@AuthenticationPrincipal UserDetails userDetails) {
+        Long userId = getCurrentUserId(userDetails);
+        return chatQueryService.myChatParticipant(userId);
+
+    }
+
 }
 
--- src/main/java/com/techup/spring/spring_be/service/chat/ChatQueryService.java
+++ src/main/java/com/techup/spring/spring_be/service/chat/ChatQueryService.java
@@ -1,7 +1,10 @@
 package com.techup.spring.spring_be.service.chat;
 
+import com.techup.spring.spring_be.domain.ChatParticipant;
 import com.techup.spring.spring_be.dto.chat.ChatMessageResponse;
+import com.techup.spring.spring_be.dto.chat.ChatParticipantResponse;
 import com.techup.spring.spring_be.repository.ChatMessageRepository;
+import com.techup.spring.spring_be.repository.ChatParticipantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +15,21 @@ import java.util.List;
 public class ChatQueryService {
 
     private final ChatMessageRepository chatMessageRepository;
+    private final ChatParticipantRepository chatParticipantRepository;
 
     public List<ChatMessageResponse> getMessages(Long communityId) {
         return chatMessageRepository
                 .findByChattingRoomIdOrderByCreatedAtAsc(communityId)
                 .stream()
                 .map(ChatMessageResponse::from)
+                .toList();
+    }
+
+    public List<ChatParticipantResponse> myChatParticipant(Long userId) {
+        List<ChatParticipant> chatParticipant = chatParticipantRepository.findByUserIdAndLeftAtIsNull(userId);
+
+        return chatParticipant.stream()
+                .map(cp -> ChatParticipantResponse.from(cp.getChattingRoom()))
                 .toList();
     }
 }
